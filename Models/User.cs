41d0d1328e068bea36f2aa1bdf1f76efdec08826
kedr4 <85400754+kedr4@users.noreply.github.com>
2024-10-06 18:29:42 +0300

    Добавьте файлы проекта.

---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace UserService.Models
+{
+    public class User
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }    
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string PasswordHash { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+using UserService.Models;
+
+namespace UserService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddDbContext<UserContext>(opt =>  opt.UseInMemoryDatabase("UserDb"));
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
