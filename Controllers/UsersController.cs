a3e3a3e7c7bd430f42f1246e186057e7e45cdf3e
kedr4 <85400754+kedr4@users.noreply.github.com>
2024-10-07 18:30:43 +0300

    added authorization check to UserController

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -29,7 +30,7 @@ namespace UserService.Controllers
 
         // GET: api/Users/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<User>> GetUser(int id)
+        public async Task<ActionResult<User>> GetUser(string id)
         {
             var user = await _context.Users.FindAsync(id);
 
@@ -44,14 +45,21 @@ namespace UserService.Controllers
         // PUT: api/Users/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutUser(int id, User user)
+        [Authorize]
+        public async Task<IActionResult> PutUser(string id, User user)
         {
             if (id != user.Id)
             {
                 return BadRequest();
             }
 
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
             _context.Entry(user).State = EntityState.Modified;
+
 
             try
             {
@@ -72,22 +80,35 @@ namespace UserService.Controllers
             return NoContent();
         }
 
+
+        /*
+         
         // POST: api/Users
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
         public async Task<ActionResult<User>> PostUser(User user)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
             return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
         }
+        
+         */
 
         // DELETE: api/Users/5
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteUser(int id)
+        [Authorize]
+        public async Task<IActionResult> DeleteUser(string id)
         {
             var user = await _context.Users.FindAsync(id);
+
+
             if (user == null)
             {
                 return NotFound();
@@ -99,7 +120,7 @@ namespace UserService.Controllers
             return NoContent();
         }
         
-        private bool UserExists(int id)
+        private bool UserExists(string id)
         {
             return _context.Users.Any(e => e.Id == id);
         }
--- Models/User.cs
+++ Models/User.cs
@@ -1,14 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
 namespace UserService.Models
 {
-    public class User:IdentityUser
+    public class User : IdentityUser
     {
-        public int Id { get; set; } 
-        public string Name { get; set; }    
+        public string Name { get; set; }
         public string Lastname { get; set; }
-        public string Email { get; set; }
         public string Role { get; set; }
-        public string UserName { get; set; } = string.Empty;
-       // public string PasswordHash { get; set; }
+        
     }
 }
